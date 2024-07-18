9f3e386588c8c8dd7217bdec71a4434233a3302d
rishigarg09 <rishigarg185@gmail.com>
2024-07-19 02:35:31 +0530

    added sendMessage and deleteUsers config

--- config/functions/bootstrap.js
+++ config/functions/bootstrap.js
@@ -3,23 +3,32 @@
 module.exports = async () => {
   const io = require('socket.io')(strapi.server, {
     cors: {
-      origin: 'http://localhost:3000',
+      origin: 'http://localhost:3000', // your frontend URL
       methods: ['GET', 'POST'],
     },
   });
 
+  const users = {};
+
   io.on('connection', (socket) => {
     console.log('a user connected');
 
+    socket.on('register', (username) => {
+      users[socket.id] = username;
+      io.emit('activeUsers', Object.values(users));
+    });
+
     socket.on('sendMessage', async (message) => {
       const newMessage = await strapi.services.message.create({
-        content: message,
-        username: socket.username,
+        content: message.content,
+        username: message.username,
       });
       io.emit('message', newMessage);
     });
 
     socket.on('disconnect', () => {
+      delete users[socket.id];
+      io.emit('activeUsers', Object.values(users));
       console.log('user disconnected');
     });
   });
