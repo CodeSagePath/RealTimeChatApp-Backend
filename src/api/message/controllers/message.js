4594b3d287605b198aed3d19cb1ccb5b10865f4d
rishigarg09 <rishigarg185@gmail.com>
2024-07-18 20:16:58 +0530

    Updated the file to include the webhook handler

--- src/api/message/controllers/message.js
+++ src/api/message/controllers/message.js
@@ -6,4 +6,12 @@
 
 const { createCoreController } = require('@strapi/strapi').factories;
 
-module.exports = createCoreController('api::message.message');
+module.exports = createCoreController('api::message.message', ({ strapi }) => ({
+  async webhook(ctx) {
+    const message = ctx.request.body;
+
+    strapi.io.to(message.room).emit('message', message);
+
+    ctx.send({ received: true });
+  },
+}));
