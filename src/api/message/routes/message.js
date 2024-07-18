d64f3eabb766ccad599b78629f440c3dd13e6eeb
rishigarg09 <rishigarg185@gmail.com>
2024-07-18 20:19:37 +0530

    Updatd the /api/messageroutes/message.js file to include the custom route for the webhook

--- src/api/message/routes/message.js
+++ src/api/message/routes/message.js
@@ -6,4 +6,23 @@
 
 const { createCoreRouter } = require('@strapi/strapi').factories;
 
-module.exports = createCoreRouter('api::message.message');
+const defaultRouter = createCoreRouter('api::message.message');
+
+const customRoutes = [
+  {
+    method: 'POST',
+    path: '/webhook/message',
+    handler: 'message.webhook',
+    config: {
+      policies: [],
+    },
+  },
+];
+
+module.exports = {
+  ...defaultRouter,
+  routes: [
+    ...defaultRouter.routes,
+    ...customRoutes,
+  ],
+};
